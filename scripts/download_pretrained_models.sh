edf444e62164c732ac6a706676041754d4612eba
김보경/DAXTF <bogyeong@lguplus.co.kr>
2023-12-10 14:20:52 +0900

    add script for setup to run

---
+++ scripts/download_pretrained_models.sh
@@ -0,0 +1,6 @@
+# git lfs install
+mkdir pretrained_models
+cd pretrained_models
+git lfs clone https://huggingface.co/runwayml/stable-diffusion-v1-5
+git lfs clone https://huggingface.co/stabilityai/sd-vae-ft-mse
+git lfs clone https://huggingface.co/zcxu-eric/MagicAnimate
